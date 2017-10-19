0613288834a61cb7f1ff2aa8c7a6eb9a048c179c
Justin Grubbs <justin@kingandpartners.com>
2017-10-19 10:08:22 -0400

    fix antigen bugs

--- home/antigen.theme.zsh
+++ home/antigen.theme.zsh
@@ -2,6 +2,7 @@
 source $DOTFILE_PATH/executables/antigen.zsh
 
 # ANTIGEN THEME
-antigen-use oh-my-zsh
-antigen-bundle arialdomartini/oh-my-git
+antigen use oh-my-zsh
+antigen bundle arialdomartini/oh-my-git
 antigen theme arialdomartini/oh-my-git-themes oppa-lana-style
+antigen apply

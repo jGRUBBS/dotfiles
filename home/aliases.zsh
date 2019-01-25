8f2a5d022e7b92c6f0eaab0b67cb0038caf24eea
Justin Grubbs <justin@kingandpartners.com>
2019-01-25 14:59:07 -0500

    add git colors and useful aliases

---
+++ home/aliases.zsh
@@ -0,0 +1,12 @@
+alias kingandpartners="open https://kingandpartners.signin.aws.amazon.com/console"
+alias grandlife="open https://grandlifehotels.signin.aws.amazon.com/console"
+alias auberge="open https://aubergeresorts.signin.aws.amazon.com/console"
+alias jgrubbs="open https://jgrubbs.signin.aws.amazon.com/console"
+
+alias weather="telnet rainmaker.wunderground.com"
+
+alias size='echo disk used in $PWD:; du -sh | awk '\''{print $1}'\'''
+
+alias count='echo files in $PWD: && find . -type fz | wc -l | awk '\''{print $1}'\'''
+
+alias fixphp="phpcbf --standard=PSR2 $1"

1d9985fc3152a07fea04f17ba7ddec88a1e0a271
Justin Grubbs <justin@kingandpartners.com>
2019-12-18 15:10:58 -0500

    update wp cli if already installed

--- scripts/wp-cli/install.sh
+++ scripts/wp-cli/install.sh
@@ -3,7 +3,7 @@
 . helper_scripts/print_format
 
 if [ -f "$DOTFILE_PATH/executables/wp" ]; then
-  skipping "WP-CLI - already installed"
+  wp cli update
 else
   doing "Installing WP-CLI"
   curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar> /dev/null 2>&1
